8ec820b2a0a129112b45aeb5298c4ce9f480ad3b
ed ben <edwardbenedict92@gmail.com>
2021-05-17 18:07:46 +0300

    feat: change profile

--- src/approuter/PrivateRouter.js
+++ src/approuter/PrivateRouter.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
-const PrivateRouter = ({ component: Component, isAuth, ...rest }) => {
+const PrivateRouter = ({ component: Component, ...rest }) => {
+  const { currentUser } = useAuth();
+
   return (
     <Route
       {...rest}
       render={(props) =>
-        isAuth ? (
+        currentUser ? (
           <Component {...props} />
         ) : (
           <Redirect
