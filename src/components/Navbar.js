c09f7e5a0a60dd00e2905bd2592a5fa8ca499a2f
ed ben <edwardbenedict92@gmail.com>
2021-05-17 17:39:59 +0300

    add: minimize navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,10 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import MailIcon from "@material-ui/icons/Mail";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import MoreIcon from "@material-ui/icons/MoreVert";
 import BookIcon from "@material-ui/icons/Book";
-import LockOpenIcon from "@material-ui/icons/LockOpen";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 
@@ -106,7 +99,6 @@ const useStyles = makeStyles((theme) => ({
 export default function PrimarySearchAppBar() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
   const { currentUser, logout } = useAuth();
   const history = useHistory();
 
@@ -114,19 +106,13 @@ export default function PrimarySearchAppBar() {
   console.log(currentUser?.email);
 
   const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
   const handleMenuClose = () => {
     setAnchorEl(null);
-    handleMobileMenuClose();
   };
 
   const handleLogout = () => {
@@ -138,97 +124,38 @@ export default function PrimarySearchAppBar() {
     history.push("/");
   };
 
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
   const menuId = "primary-search-account-menu";
   const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleLogout}>Logout</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
     <>
       {currentUser?.email ? (
         <Menu
-          anchorEl={mobileMoreAnchorEl}
+          anchorEl={anchorEl}
           anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          id={mobileMenuId}
+          id={menuId}
           keepMounted
           transformOrigin={{ vertical: "top", horizontal: "right" }}
-          open={isMobileMenuOpen}
-          onClose={handleMobileMenuClose}
+          open={isMenuOpen}
+          onClose={handleMenuClose}
         >
-          <MenuItem>
-            <IconButton aria-label="show 11 new notifications" color="inherit">
-              <Badge badgeContent={11} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <p>Notifications</p>
-          </MenuItem>
-          <MenuItem onClick={handleLogout}>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <LockOpenIcon />
-            </IconButton>
-            <p>Logout</p>
-          </MenuItem>
-          <MenuItem onClick={handleLogout}>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <LockOpenIcon />
-            </IconButton>
-            <p>Profile</p>
-          </MenuItem>
-          <MenuItem onClick={handleProfileMenuOpen}>
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="primary-search-account-menu"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <p>Profile</p>
-          </MenuItem>
+          <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
         </Menu>
       ) : (
         <Menu
-          anchorEl={mobileMoreAnchorEl}
+          anchorEl={anchorEl}
           anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          id={mobileMenuId}
+          id={menuId}
           keepMounted
           transformOrigin={{ vertical: "top", horizontal: "right" }}
-          open={isMobileMenuOpen}
-          onClose={handleMobileMenuClose}
+          open={isMenuOpen}
+          onClose={handleMenuClose}
         >
-          <Link to="/login" className={classes.maxWidthLogReg}>
-            <MenuItem onClick={handleLogout}>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <LockOpenIcon />
-              </IconButton>
-              <p>Login</p>
-            </MenuItem>
-          </Link>
-          <Link to="/register" className={classes.maxWidthLogReg}>
-            <MenuItem>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <LockOpenIcon />
-              </IconButton>
-              <p>Register</p>
-            </MenuItem>
-          </Link>
+          <MenuItem onClick={handleMenuClose}>
+            <Link to="/login">Login</Link>
+          </MenuItem>
+          <MenuItem onClick={handleLogout}>
+            <Link to="/register">Register</Link>
+          </MenuItem>
         </Menu>
       )}
     </>
@@ -253,58 +180,22 @@ export default function PrimarySearchAppBar() {
             </Typography>
           </Link>
           <div className={classes.grow} />
-          {currentUser?.email ? (
-            <div className={classes.sectionDesktop}>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-          ) : (
-            <div className={classes.sectionDesktop}>
-              <div className={classes.loginRegDiv}>
-                <Link to="/login" className={classes.login}>
-                  Login
-                </Link>
-                <Link to="/register" className={classes.register}>
-                  Register
-                </Link>
-              </div>
-            </div>
-          )}
-          <div className={classes.sectionMobile}>
+
+          <div>
             <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
               aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
+              onClick={handleProfileMenuOpen}
               color="inherit"
             >
-              <MoreIcon />
+              <AccountCircle />
             </IconButton>
           </div>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
+
       {renderMenu}
     </div>
   );
