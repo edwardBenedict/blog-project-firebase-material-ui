baef2f7a1c564447644d2d925ec7818c2500d7b1
ed ben <edwardbenedict92@gmail.com>
2021-05-30 14:50:06 +0300

    feat: add fireblog gif and profilepage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,73 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { useAuth } from "../contexts/AuthContext";
 
-const Profile = () => {
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    maxWidth: 500,
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 25,
+  },
+
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  mainDiv: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 500,
+    marginTop: 100,
+  },
+  image: {
+    borderRadius: "50%",
+    width: "100px",
+  },
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
+
+  const { currentUser } = useAuth();
+
   return (
-    <div style={{ marginTop: 70 }}>
-      <h2>Profile Page</h2>
+    <div className={classes.mainDiv}>
+      <Card className={classes.root}>
+        <img
+          src={currentUser?.photoURL}
+          className={classes.image}
+          alt="profile"
+        />
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Display Name
+          </Typography>
+          <Typography variant="h5" component="h2">
+            {currentUser?.displayName || "Not Found!"}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Email
+          </Typography>
+          <Typography variant="body2" component="p">
+            {currentUser?.email || "Not Found!"}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
-};
-
-export default Profile;
+}
